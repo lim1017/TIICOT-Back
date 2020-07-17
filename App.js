2e4b01f1a0174ff68fb6bf19c8c6d8086d98947f
tom <lim1017@hotmail.com>
2020-07-17 13:20:54 -0400

    installed cors

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 const express = require("express");
+var cors = require('cors')
+
 
 const app = express();
 
 const get = require("./routes/get");
 const ping = require("./routes/ping");
 
+app.use(cors())
 
 app.use("/api", get());
 app.use("/api", ping());
